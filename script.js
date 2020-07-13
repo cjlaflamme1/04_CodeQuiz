378e0990e0847bc5868896c2b904aef851a00af3
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-07-13 08:56:01 -0400

    Modified ?-bank into objects in array for easy nav

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const answerButton2 = document.getElementById('answerButton2');
 const answerButton3 = document.getElementById('answerButton3');
 const answerButton4 = document.getElementById('answerButton4');
 // Quiz questions in nested objects
-const quizQuestions = {
-    question1: {
+const quizQuestions = [
+    {
         questionTitle: "This is a test question?",
         answer1: "No",
         answer2: "Maybe",
@@ -16,7 +16,7 @@ const quizQuestions = {
         answer4: "Yuppers",
         correctAnswer: 4
     },
-    question2: {
+    {
         questionTitle: "This is also a test question?",
         answer1: "Nej",
         answer2: "Maybe in Swedish",
@@ -24,16 +24,24 @@ const quizQuestions = {
         answer4: "inte so micha",
         correctAnswer: 3
     }
+]
+let quizTimer = 60;
+let currentQuestionCount = 0;
+// This is the function that modifies the quiz content and progresses the question counter.
+function questionModifier(i) {
+    if (quizTimer > 0) {
+    questionTitle.innerText = quizQuestions[i].questionTitle;
+    answerButton1.innerText = quizQuestions[i].answer1;
+    answerButton2.innerText = quizQuestions[i].answer2;
+    answerButton3.innerText = quizQuestions[i].answer3;
+    answerButton4.innerText = quizQuestions[i].answer4;
+    currentQuestionCount++;
+    }
+    else {
+        return;
+    }
 }
-// How on earth do I get this to scroll through the object depending on the current question number?
-function questionModifier() {
-    questionTitle.innerText = quizQuestions.question1.questionTitle;
-    answerButton1.innerText = quizQuestions.question1.answer1;
-    answerButton2.innerText = quizQuestions.question1.answer2;
-    answerButton3.innerText = quizQuestions.question1.answer3;
-    answerButton4.innerText = quizQuestions.question1.answer4;
-}
-questionModifier();
+
 
 
 // Begin quiz button function
@@ -41,7 +49,9 @@ startQuizButton.addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('introCard').classList.add('d-none');
     document.getElementById('questionCard').classList.remove('d-none');
+    // start timer
     // call function to generate questions
+    questionModifier(currentQuestionCount);
     // increase question count
 })
 
