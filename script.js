761863a9b456206dfb6243d5ece7418b0b9240d0
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-07-13 09:53:55 -0400

    Added check for correct answers.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const startQuizButton = document.getElementById('startQuizButton');
 // Hooks for quiz card
 const questionTitle = document.getElementById('questionTitle');
-const answerButton1 = document.getElementById('answerButton1');
-const answerButton2 = document.getElementById('answerButton2');
-const answerButton3 = document.getElementById('answerButton3');
-const answerButton4 = document.getElementById('answerButton4');
-// Quiz questions in nested objects
+const answerButton1 = document.getElementById('1');
+const answerButton2 = document.getElementById('2');
+const answerButton3 = document.getElementById('3');
+const answerButton4 = document.getElementById('4');
+// Quiz questions in Array of objects.
 const quizQuestions = [
     {
         questionTitle: "This is a test question?",
@@ -14,7 +14,7 @@ const quizQuestions = [
         answer2: "Maybe",
         answer3: "Probably",
         answer4: "Yuppers",
-        correctAnswer: 4
+        correctAnswer: "4"
     },
     {
         questionTitle: "This is also a test question?",
@@ -22,7 +22,7 @@ const quizQuestions = [
         answer2: "Maybe in Swedish",
         answer3: "Ja",
         answer4: "inte so micha",
-        correctAnswer: 3
+        correctAnswer: "3"
     }
 ]
 let quizTimer = 60;
@@ -35,10 +35,38 @@ function questionModifier(i) {
     answerButton2.innerText = quizQuestions[i].answer2;
     answerButton3.innerText = quizQuestions[i].answer3;
     answerButton4.innerText = quizQuestions[i].answer4;
-    currentQuestionCount++;
+    // currentQuestionCount++;
     }
     else {
         return;
+    }
+}
+function answerQuestion(event) {
+    let i = currentQuestionCount;
+    const targetButton = event.target.classList.contains('answerButton');
+    console.log('You (tried) to click an answer!');
+    if (targetButton) {
+        // This function executes if they actually click a button on the screen.
+        const targetAnswerID = event.target.id;
+        console.log(targetAnswerID);
+        if (targetAnswerID === quizQuestions[i].correctAnswer) {
+            // This function executes if they click the correct button.
+            console.log('You are correct!')
+            currentQuestionCount++;
+            questionModifier(currentQuestionCount);
+            // Add answer feedback text to HTML
+        } 
+        else if (targetAnswerID !== quizQuestions[i].correctAnswer) {
+            // This function executes if they click the incorrect button.
+            console.log('WROOOONG!');
+            currentQuestionCount++;
+            questionModifier(currentQuestionCount);
+            // Add answer feedback text to HTML
+            // subtract time from quiz timer
+        }
+    } else {
+        // this executes if they click anywhere on the screen that is not a button.
+        return
     }
 }
 
@@ -52,7 +80,6 @@ startQuizButton.addEventListener('click', function(event) {
     // start timer
     // call function to generate questions
     questionModifier(currentQuestionCount);
-    // increase question count
 })
-
 // Add event listener for answering questions
+document.querySelector('div#questionCard').addEventListener('click', answerQuestion);
